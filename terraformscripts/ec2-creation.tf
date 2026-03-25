72378c2ac20b5fc3cbea60f0d87af938b44dcd78
AdItYa2806-ops <adityajharwal.devops@gmail.com>
2026-03-25 20:15:45 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,15 +1,13 @@
 provider "aws"{
-region = "ap-south-2"
+region ="ap-south-1"
 }
 
-
-resource "aws_instance" "AWSEC2Instance" {
- ami = "ami-090b9c8aa1c84aefc"
- instance_type = "t3.micro"
- key_name = "devopsfeb2026"
- security_groups = ["launch-wizard-2"]
- tags = {
- Name = "Terraform Server"
- }
+resource "aws_instance" "AWSEC2Instance"{
+    ami = "ami-0f559c3642608c138"
+    instance_type = "t2.micro"
+    key_name = "ansible"
+    security_groups = ["launch-wizard-1"]
+    tags = {
+    Name = "TerraformServer"
+    }
 }
-
